8b2b43a5233f8355faec568e38594751ecd219e2
Sadhu <39456032+FE-Sadhu@users.noreply.github.com>
2023-05-11 14:05:03 +0800

    feat: 1.1~1.2

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private int mCount = 0;
+    private TextView tvView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "Hello World.");
+        tvView = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (tvView != null) {
+            tvView.setText(Integer.toString(mCount));
+        }
+    }
+}
